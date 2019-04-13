e1808243bc33f8323372f00b3f6fedf27b2855cf
eunoia_1729 <bkumarraju@gmail.com>
2019-04-13 23:23:32 +0530

    Deleted extra files

--- static_data/extract.py
+++ static_data/extract.py
@@ -13,11 +13,8 @@ with open('stops.txt') as fin:
         	continue
         my_list = line.split(",")
         stops[my_list[2]] = int(my_list[0])
-        stops_name[int(my_list[0])] = my_list[2] 
-"""
-for k,v in stops.items():
-	print(str(k) + ' : '+ str(v))
-"""
+        stops_name[int(my_list[0])] = my_list[2]
+
 flag = 0
 with open('trips.txt') as fin:
     for line in fin:
@@ -25,21 +22,16 @@ with open('trips.txt') as fin:
         	flag = 1
         	continue
         my_list = line.split(",")
-        trips[int(my_list[2])] = int(my_list[0]) 
+        trips[int(my_list[2])] = int(my_list[0])
 
 flag = 0
-""""
-for k,v in trips.items():
-	print(str(k) + ' : '+ str(v))
-"""
-print('route of 0 is : '+ str(trips[0]))
 with open('stop_times.txt') as fin:
     for line in fin:
         if flag == 0:
         	flag = 1
         	continue
         my_list = line.split(",")
-        #print(my_list[0] + ' : '+ my_list[3])
+
         if trips[int(my_list[0])] not in routes:
         	routes[trips[int(my_list[0])]] = []
         if int(my_list[3]) not in routes[trips[int(my_list[0])]]:
@@ -52,12 +44,9 @@ with open('routes.txt') as fin:
         	continue
         my_list = line.split(",")
         routes_name[int(my_list[3])] = my_list[1]
-#for k in routes.keys():
-#	print(routes[k])
-#print('The End')
 shelfFile['stops'] = stops;
 shelfFile['routes'] = routes;
-shelfFile['trips'] = trips;	
+shelfFile['trips'] = trips;
 shelfFile['stops_name'] = stops_name;
 shelfFile['routes_name'] = routes_name;
 shelfFile.close()
